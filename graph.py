fb634a2e9aac4453c1f225fba50df12f42f82d69
piearty <piearty@users.noreply.github.com>
2018-12-17 17:38:35 -0500

    did a lot that i probably should've documented earlier
    - made a whole damn CLASS so i could make connections
    - defined a function that slotted in given attributes into something parsable by add_edges_from
    - made a lot of dictionary variables so i don't have to type things multiple times

--- graph.py
+++ graph.py
@@ -1,20 +1,47 @@
+#graph maker
 import networkx as nx
+#converts graph to .dot file to be written by graphviz
 from networkx.drawing.nx_pydot import write_dot
 import pydot
-from itertools import product
 
-g = nx.MultiDiGraph(rpg="The Veil")
+g = nx.MultiDiGraph(label="The Veil")
 
-people = ['Ribbon', 'Calder', 'Thiago', 'Aliquot']
-combos = []
+colors = {'blue':'#545b90', 'yellow':'#fbfd54', 'green':'#438718', 'red':'#a90f0f', 'purple':'#954ed2', 'orange':'#ea8218'}
 
-for r in product(people, repeat=2):
-    if r[0] != r[1]:
-        combos.append(r)
-
-print(combos)
+emotions = {'sad':{'color':colors['blue']}, 'joyful':{'color':colors['yellow']}, 'scared':{'color':colors['green']}, 'angry':{'color':colors['red']}, 'powerful':{'color':colors['purple']}, 'peaceful':{'color':colors['orange']}}
+labelColors = {'sadLabel':{'fontcolor':colors['blue']}, 'joyfulLabel':{'fontcolor':colors['yellow']}, 'scaredLabel':{'fontcolor':colors['green']}, 'angryLabel':{'fontcolor':colors['red']}, 'powerfulLabel':{'fontcolor':colors['purple']}, 'peacefulLabel':{'fontcolor':colors['orange']}}
+toward = {'arrowhead':'odot'}
 
 
-g.add_edges_from(combos)
+class Connection:
+    def __init__(self, source, target, state, arrow=None, label=None, fontcolor=None):
+        self.source = source
+        self.target = target
+        self.state = state
+        self.arrow = arrow
+        self.label = label
+        self.fontcolor = fontcolor
+
+    def to_dict(self):
+        return dict(
+            [(k, v) for k, v in self.__dict__.items() if not k.startswith('_')]
+        )
+
+RibbonCalder = Connection('Ribbon', 'Calder', 'peaceful', None, {'label':'2'}, labelColors['peacefulLabel'])
+CalderRibbon = Connection('Calder', 'Ribbon', 'scared', toward)
+
+def listConnection(singleConnection):
+    names = [singleConnection.source, singleConnection.target]
+    attributes = {}
+    attributes.update(emotions[singleConnection.state])
+    connectDict = singleConnection.to_dict()
+    for attribute in connectDict:
+        if connectDict[attribute] and attribute not in ('source', 'target', 'state'):
+            print(connectDict[attribute])
+            attributes.update(connectDict[attribute])
+    names.append(attributes)
+    return names
+
+g.add_edges_from([listConnection(RibbonCalder), listConnection(CalderRibbon)])
 
 graphdot = write_dot(g,'multi.dot')
