32a82362e957d0f19cd93a4bb111feb17a1fd68c
piearty <piearty@users.noreply.github.com>
2018-12-19 14:58:54 -0500

    adding documentation
    also realized i don't need a separate add and save buttons so merged them
    
    made it so when delete button is clicked it also disabled if there's no edgeLIst left

--- graph.py
+++ graph.py
@@ -1,39 +1,39 @@
-#this file does the graph writing and processing using networkx and graphviz
-#right now it's very specialized to work for making relationship maps for The Veil prg campaign I'm in
-#(i'm a huge nerd)
+# this file does the graph writing and processing using networkx and graphviz
+# right now it's very specialized to work for making relationship maps for The Veil prg campaign I'm in
+# (i'm a huge nerd)
 
-#graph maker
+# graph maker
 import networkx as nx
-#converts graph to .dot file to be written by graphviz
+# converts graph to .dot file to be written by graphviz
 from networkx.drawing.nx_pydot import to_pydot, write_dot
 import pydot
 
-#defines a directed multigraph called 'The Veil':
-#a graph that can have multiple lines between two nodes, and has arrows
+# defines a directed multigraph called 'The Veil':
+# a graph that can have multiple lines between two nodes, and has arrows
 g = nx.MultiDiGraph(label="The Veil")
 
-#colors for the emotional states. 
-#these are dictionaries so that the add_edges_from networkx function can read them properly
-colors = {'blue':'#545b90', 'yellow':'#f6e53d', 'green':'#438718', 'red':'#a90f0f', 'purple':'#954ed2', 'orange':'#ea8218'}
+# colors for the emotional states. 
+# these are dictionaries so that the add_edges_from networkx function can read them properly
+colors = {'blue':'#002ba1', 'yellow':'# f6e53d', 'green':'# 438718', 'red':'# a90f0f', 'purple':'# 954ed2', 'orange':'# ea8218'}
 
-#states and obligationColors that are linked to the aforementioned colors
-#they are dictionaries so i can reference their names instead of the color when using attributes. like states[sad] instead of colors[blue]
-#subject to change as this gets more general
+# states and obligationColors that are linked to the aforementioned colors
+# they are dictionaries so i can reference their names instead of the color when using attributes. like states[sad] instead of colors[blue]
+# subject to change as this gets more general
 states = {'sad':{'color':colors['blue']}, 'joyful':{'color':colors['yellow']}, 'scared':{'color':colors['green']}, 'angry':{'color':colors['red']}, 'powerful':{'color':colors['purple']}, 'peaceful':{'color':colors['orange']}}
 obligationColors = {'sad':{'fontcolor':colors['blue']}, 'joyful':{'fontcolor':colors['yellow']}, 'scared':{'fontcolor':colors['green']}, 'angry':{'fontcolor':colors['red']}, 'powerful':{'fontcolor':colors['purple']}, 'peaceful':{'fontcolor':colors['orange']}}
-#determines arrowhead style (whether it's an arrow or an empty dot)
-#indicates whether the emotion is felt about someone or for someone's sake
-#"i am mad at you" = arrow
-#"i am scared for your sake" = dot
+# determines arrowhead style (whether it's an arrow or an empty dot)
+# indicates whether the emotion is felt about someone or for someone's sake
+# "i am mad at you" = arrow
+# "i am scared for your sake" = dot
 tenors = {'for':{'arrowhead':'odot'}, 'toward':{'arrowhead':'normal'}}
-#determines edge (line) style (whether it's dotted or a bold line)
-#indicates how strong relationship is
-#tenuous/not fully formed = dotted
-#strong = bold
+# determines edge (line) style (whether it's dotted or a bold line)
+# indicates how strong relationship is
+# tenuous/not fully formed = dotted
+# strong = bold
 strength = {'tenuous':{'style':'dotted'}, 'strong':{'style':'bold'}}
 
-#a class that creates objects that can be used to make edges with the add_edges_from function
-#attributes are all attributes that the edge will have
+# a class that creates objects that can be used to make edges with the add_edges_from function
+# attributes are all attributes that the edge will have
 class Connection:
     def __init__(self, source, target, state, obligation=None, tenor=None, line='strong'):
         self.source = source
@@ -43,27 +43,27 @@ class Connection:
         self.tenor = tenor
         self.line = line
 
-    # puts attributes in a dictionary to be read as needed
+    #  puts attributes in a dictionary to be read as needed
     def to_dict(self):
         return dict(
-            #instance attributes only
+            # instance attributes only
             [(k, v) for k, v in self.__dict__.items() if not k.startswith('_')]
         )
 
 RibbonCalder = Connection('Ribbon', 'Calder', 'joyful')
 
-#turns object attributes into a list that can be readable by add_edges_from
+# turns object attributes into a list that can be readable by add_edges_from
 def listConnection(singleConnection):
-    #initial list ['Ribbon', 'Calder']
+    # initial list ['Ribbon', 'Calder']
     names = [singleConnection.source, singleConnection.target]
-    #an empty dictionary
+    # an empty dictionary
     attributes = {}
-    #calls the .to_dict() method in the singleConnection object and saves dictionary in connectDict
+    # calls the .to_dict() method in the singleConnection object and saves dictionary in connectDict
     connectDict = singleConnection.to_dict()
-    #for attribute key in connectDict
+    # for attribute key in connectDict
     for attribute in connectDict:
-        #if it's not already in there
-         #merges attribute to attributes dictionary according to attribute
+        # if it's not already in there
+         # merges attribute to attributes dictionary according to attribute
         if connectDict[attribute] and attribute not in ('source', 'target'):
             if attribute is 'state':
                 attributes.update(states[singleConnection.state])
@@ -75,21 +75,15 @@ def listConnection(singleConnection):
             elif attribute is 'line':
                 attributes.update(strength[singleConnection.line])
 
-    #adds dictionary to the names list
-    #looks like ['Ribbon', 'Calder' {'color': '#ea8218', other attributes}]
+    # adds dictionary to the names list
+    # looks like ['Ribbon', 'Calder' {'color': '# ea8218', other attributes}]
     names.append(attributes)
     return names
 
-#function to shorten typing bc i'm lazy
-#uses add_edges_from to create an edge
+# function to shorten typing bc i'm lazy
+# uses add_edges_from to create an edge
 def addEdge(singleConnection):
     g.add_edges_from([listConnection(singleConnection)])
 
 def removeEdge(singleConnection):
     g.remove_edge(*listConnection(singleConnection)[:2])
-
-#turns the graph g into a pydot format
-p = to_pydot(g)
-p.write_dot('test.dot')
-#writes pydot format graph into a png
-p.write_png('test.png')
--- gui.py
+++ gui.py
@@ -1,86 +1,115 @@
-#this creates the gui to interface with the graph.py function
+# this creates the gui to interface with the graph.py function
 
-#lets app accept command line arguments
+# lets app accept command line arguments
 import sys
 
-#imports the gui aspect of pyqt
+# imports the gui aspect of pyqt
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-#imports graph.py
+# imports graph.py
 import graph
 
 
-#class that is the gui
+# class that is the gui
 class VeilGUI(QDialog):
 
    def __init__(self, parent = None):
       super(VeilGUI, self).__init__(parent)
       
-      #names gui window
+      # names gui window
       self.setWindowTitle('Veil Relationship Map GUI')
 
-      #list to contain edges created later
+      # list to contain edges created later
       self.edgesList = []
 
-      #dict to contain radio boxes created later
+      # dict to contain radio boxes created later
       self.radioButtonsDict = {}
 
-      #list to contain saved edges created later
+      # list to contain saved edges created later
       self.savedEdgesList = []
 
-      #creates the grid where the widgets will be placed
+      # creates the grid where the widgets will be placed
       grid = QGridLayout()
 
-      #adds widgets
+      # save_buttons widgets
+      # names widget (text field for source and target/node names)
       grid.addWidget(self.namesBoxGroup(), 0, 0)
+      # obligations widget (integer spin box for obligation/edge label)
       grid.addWidget(self.obligationsBoxGroup(), 1,0)
+      # states widget (radio buttons for states/color of label)
       grid.addWidget(self.statesBoxGroup(), 0, 1)
+      # tenor widget (radio buttons for tenors/arrowhead shape)
       grid.addWidget(self.tenorBoxGroup(), 1, 1)
+      # lines widget (radio buttons for strength of relationship/edge style)
       grid.addWidget(self.lineBoxGroup(), 2, 0)
-      grid.addWidget(self.addButtonGroup(), 2, 1)
+      # save_button button (for save connection)
+      grid.addWidget(self.saveButtonGroup(), 2, 1)
+      # map widget (to show png)
       grid.addWidget(self.showMap(), 0, 3, 3, 3)
+      # combo box widget (to show edge list)
       grid.addWidget(self.edgeComboBoxGroup(), 3, 0, 1, 2)
+      # save button (for writing to png)
+    #  grid.addWidget(self.saveButtonGroup(), 4, 1)
+      # edge buttons (for editing or deleting edges)
       grid.addWidget(self.edgeButtonsGroup(), 4,0)
-      grid.addWidget(self.saveButtonGroup(), 4, 1)
 
 
-      #creates grid
+      # creates grid layout
       self.setLayout(grid)
 
-   #
+   # creates radio buttons automatically instead of having to list them one by one
+   # takes the name of the group (so you can reference each radio button group separately),
+   # label text that the radio button group will be labeled
+   # list of items you want to be made into radio buttons
    def radioMaker(self, group, label, buttonsList):
+      # creates group box
       radioBox = QGroupBox(label)
+      # makes a vertical layout
       radioLayout = QVBoxLayout()
+      # for button in list, make radio button and put it in radioButtons list
       radioButtons = [QRadioButton(b) for b in buttonsList]
+      # set the first radio button as the default button checked
       radioButtons[0].setChecked(True)
+      # makes a smol (one key) dictionary, radioButtonsDict
+      # with a key determined by the argument passed to 'group' parameter
+      # and its value being a QButtonGroup
+      # effectively naming the QButtonGroup and allowing it to be uniquely referenced outside of this method
+      # # # # (QButtonGroup.checkedButton().text() outputs the text of the button that was selected
+      # # # # within QButtonGroup
+      # # # # instead of treating each button as separate)
       self.radioButtonsDict.update({group:QButtonGroup()})
+      # for every button in radioButtons
       for i, self.button in enumerate(radioButtons):
+         # put the widget in radioLayout
          radioLayout.addWidget(self.button)
+         # and adds a button to the QButtonGroup in the radioButtonsDict
          self.radioButtonsDict[group].addButton(radioButtons[i])
 
+      #set the group box's layout as radioLayout
       radioBox.setLayout(radioLayout)
 
+      #returns the group box
       return radioBox
 
-   def addButtonGroup(self):
-      #creates button labeled add
-      addingButton = QPushButton("Add Connection")
-      addingButton.setCheckable(True)
+   def saveButtonGroup(self):
+      # creates button labeled Save Connection
+      saveButton = QPushButton("Save Connection")
+      saveButton.setCheckable(True)
 
-      #box containing add button
-      addingButtonBox = QDialogButtonBox()
-      addingButtonBox.addButton(addingButton, QDialogButtonBox.ActionRole)
+      # box containing save button
+      saveButtonBox = QDialogButtonBox()
+      saveButtonBox.addButton(saveButton, QDialogButtonBox.ActionRole)
 
-      #when clicked, run the add_button_clicked, edit_button_clicked, and del_button_clicked methods respectively
-      addingButton.clicked.connect(self.add_button_clicked)
+      # when clicked, run the save_button_clicked method respectively
+      saveButton.clicked.connect(self.save_button_clicked)
 
-      return addingButtonBox
+      return saveButtonBox
 
    def edgeButtonsGroup(self):
-      #creates buttons labeled edit, and delete
+      # creates buttons labeled edit, and delete, but makes it so they can't be clicked on yet
       self.editingButton = QPushButton("Edit")
       self.editingButton.setCheckable(True)
       self.editingButton.setEnabled(False)
@@ -89,118 +118,139 @@ class VeilGUI(QDialog):
       self.deletingButton.setCheckable(True)
       self.deletingButton.setEnabled(False)
 
-      #box containing edit, and delete buttons (these modify the edges)
+      # box containing edit and delete buttons (these modify the edges)
       edgeButtonsBox = QDialogButtonBox()
       edgeButtonsBox.addButton(self.editingButton, QDialogButtonBox.ActionRole)
       edgeButtonsBox.addButton(self.deletingButton, QDialogButtonBox.ActionRole)      
       
-  #    self.editingButton.clicked.connect(self.edit_button_clicked)
+      # below is code that's not implemented yet
+      # self.editingButton.clicked.connect(self.edit_button_clicked)
+
+      # when delete button is clicked, run the delete button method
+      # also run buttons_enabled method
       self.deletingButton.clicked.connect(self.del_button_clicked)
+      self.deletingButton.clicked.connect(self.buttons_enabled)
 
       return edgeButtonsBox
-      
 
-   def saveButtonGroup(self):
-
-      #creates button labeled save
-      savingButton = QPushButton("Save")
-      savingButton.setCheckable(True)
-
-      #box containing save button
-      self.saveButtonBox = QDialogButtonBox()
-      self.saveButtonBox.addButton(savingButton, QDialogButtonBox.ActionRole)
-
-      #when clicked, runs the save_button_clicked method
-      savingButton.clicked.connect(self.save_button_clicked)
-
-      return self.saveButtonBox
-
-   #box for the names of the source and target
+   # box for the names of the source and target
+   # this determines the names of the first and second node, and creates a default edge/line between them
    def namesBoxGroup(self):
-      #labels box
+      # labels box
       namesBox = QGroupBox('Names')
-      #creates source line field and labels it
+      # creates source line field and labels it
       sourceLabel = QLabel('Starting Person')
       self.sourceLine = QLineEdit()
-      #creates target source line field and labels it
+      # creates target source line field and labels it
       targetLabel = QLabel('Ending Person')
       self.targetLine = QLineEdit()
-      #creates layout
+      # creates layout
       namesLayout = QVBoxLayout()
-      #adds line fields and labels to layout
+      # save_buttons line fields and labels to layout
       namesLayout.addWidget(sourceLabel)
       namesLayout.addWidget(self.sourceLine)
       namesLayout.addWidget(targetLabel)
       namesLayout.addWidget(self.targetLine)
       
-      #sets box as layout
+      # sets box as layout
       namesBox.setLayout(namesLayout)
 
       return namesBox
 
+   # creates a radio button group box labeled State with the listed strings
+   # can be referenced with self.radioButtonsDict['stateGroup']
+   # this is for the emotional states that a character can feel toward another
+   # in practical terms, changes the color of the edges
+   # between yellow (joyful), red (angry), purple (powerful), orange (peaceful), blue (sad), and green (scared)
    def statesBoxGroup(self):
       states = ['joyful', 'angry', 'powerful', 'peaceful', 'sad', 'scared']
       
       return self.radioMaker('stateGroup', 'State', states) 
 
+   # creates a radio button group box labeled Tenor of state with the listed strings
+   # can be referenced with self.radioButtonsDict['tenorGroup']
+   # this is for whether the emotional state is toward or for the second person
+   # practically, it changes the arrowhead shape between normal arrow (toward) and empty dot (for)
    def tenorBoxGroup(self):
       tenors = ['toward', 'for']
 
       return self.radioMaker('tenorGroup', 'Tenor of state', tenors)
 
+   # creates a radio button group box labeled Strength of relationship with the listed strings
+   # can be referenced with self.radioButtonsDict['lineGroup']
+   # this is for the strength of the relationship
+   # practically, it changes the edge style between bold (strong) and dotted (tenuous)
    def lineBoxGroup(self):
       lines = ['strong', 'tenuous']
 
       return self.radioMaker('lineGroup', 'Strength of relationship', lines)
 
+   # spin box for the obligations owed to first person
+   # a spin box has an integer in it with an up arrow and a down arrow
+   # this one determines the edge's label
    def obligationsBoxGroup(self):
+      #group box label
       obligationBox = QGroupBox('Obligations owed')
+      #label of actual spin box
       self.obligationLabel = QLabel('Enter integer:')
+      #creates spin box
       self.obligationSpinBox = QSpinBox()
+      #vertical layout
       obligationBoxLayout = QVBoxLayout()
+      #save_buttons spin box to layout
       obligationBoxLayout.addWidget(self.obligationSpinBox)
-      
+      #sets group box layout as vertical layout
       obligationBox.setLayout(obligationBoxLayout)
 
+      #returns group box
       return obligationBox
 
+   # combo box to list the edges
+   # a combo box is a dropdown that you can select from
+   # this one lets you select from different edges to edit or delete as you choose
    def edgeComboBoxGroup(self):
+      # creates group box and labels it
       edgeComboBox = QGroupBox('List of connections')
+      # creates combo box
       self.edgeCombo = QComboBox()
+      # horizontal layout
       edgeComboBoxLayout = QHBoxLayout()
+      # save_buttons combo box to layout
       edgeComboBoxLayout.addWidget(self.edgeCombo)
+      #sets group box layout to horizontal layout
       edgeComboBox.setLayout(edgeComboBoxLayout)
 
+      # if the index has changed, run self.buttons_enabled
       self.edgeCombo.currentIndexChanged.connect(self.buttons_enabled)
 
       return edgeComboBox
 
-   
+   # map box to show the map's progress
    def showMap(self):
       mapBox = QGroupBox('Map')
       self.mapPic = QLabel()
       self.mapPic.setPixmap(QPixmap("test.png"))
-      self.mapPic.show()
       mapBoxLayout = QVBoxLayout()
       mapBoxLayout.addWidget(self.mapPic)      
-      #sets box as layout
+      # sets box as layout
       mapBox.setLayout(mapBoxLayout)
-
-      self.mapPic.show()
+      
 
       return mapBox
    
-   #### METHODS ####
-
+   # # # #  METHODS # # # #
 
    def buttons_enabled(self):
       if self.edgesList:
          self.editingButton.setEnabled(True)
          self.deletingButton.setEnabled(True)
+      if self.edgesList == [None]:
+         self.editingButton.setEnabled(False)
+         self.deletingButton.setEnabled(False)
 
 
-   def add_button_clicked(self):
-      #sets variables then clears the text fields when applicable
+   def save_button_clicked(self):
+      # sets variables then clears the text fields when applicable
       source = self.sourceLine.text()
       self.sourceLine.setText('')
       
@@ -223,7 +273,16 @@ class VeilGUI(QDialog):
          self.edgesList.append(connectionObject)
          connectionsList = graph.listConnection(connectionObject)
          self.edgeCombo.addItem(connectionsList[0]+' to '+connectionsList[1])
+         for edge in self.edgesList:
+            if edge not in self.savedEdgesList:
+               self.savedEdgesList.append(edge)
+               graph.addEdge(edge)
+               p = graph.to_pydot(graph.g)
+               p.write_png('test.png', prog='dot')
+               self.mapPic.setPixmap(QPixmap("test.png"))
 
+
+   # this doesn't quite work yet BUT IT WILL!! 
    def del_button_clicked(self):
       refNum = self.edgeCombo.currentIndex()
       print(refNum)
@@ -236,14 +295,8 @@ class VeilGUI(QDialog):
       else:
          self.edgesList[refNum] = None
       self.edgeCombo.removeItem(refNum)
-   
-   def save_button_clicked(self):
-      for edge in self.edgesList:
-         if edge not in self.savedEdgesList:
-            self.savedEdgesList.append(edge)
-            graph.addEdge(edge)
-            p = graph.to_pydot(graph.g)
-            p.write_png('test.png', prog='dot')
+      print(self.edgesList)
+      self.buttons_enabled()
 
 
 def main():
